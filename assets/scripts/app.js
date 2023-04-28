99e55d262514d17c0f3a33e448b378a90582adc9
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-04-27 17:00:42 -0700

    Functionality is all Done

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,56 @@
 const API_KEY = 'a4ba92dfa1b035b320ae6ecc40897eb0';
 const searchButton = $('#search-form button');
-const tabLi = $('div[role="tablist"]');
+const tabs = $('div[role="tablist"]');
+
+let savedCities = [];
 let fiveDayArray = [];
 let currentWeather;
 
 const iconImage = (imgId) =>
   `https://openweathermap.org/img/wn/${imgId}@2x.png`;
 
+async function fetchApi(cityName) {
+  const res = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${API_KEY}`,
+  );
+  const data = await res.json();
+  const newCity = {
+    name: cityName,
+    lat: data[0].lat,
+    lon: data[0].lon,
+  };
+
+  const currentWeatherRes = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${newCity.lat}&lon=${newCity.lon}&appid=${API_KEY}&units=imperial`,
+  );
+
+  currentWeather = await currentWeatherRes.json();
+
+  const fiveDayWeatherRes = await fetch(
+    `http://api.openweathermap.org/data/2.5/forecast?lat=${newCity.lat}&lon=${newCity.lon}&appid=${API_KEY}&units=imperial`,
+  );
+
+  const weather = await fiveDayWeatherRes.json();
+
+  fiveDayArray = weather.list.filter((day) => {
+    return day.dt_txt.includes('15:00:00');
+  });
+
+  return newCity;
+}
+
+function saveCity(cityObj) {
+  const previous = localStorage.getItem('forecast');
+  const olderCities = previous ? JSON.parse(previous) : [];
+  olderCities.push(cityObj);
+  savedCities.push(cityObj);
+  localStorage.setItem('forecast', JSON.stringify(olderCities));
+}
+
+// Function to loop through the fiveDayForcast array and dynamically create the data
 function createFiveDayForecast() {
   let innerHTML = ``;
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < fiveDayArray.length; i++) {
     innerHTML += `
       <div tabindex="0" class="single-day-forecast">
         <p class="dynamic-date">${dayjs(fiveDayArray[i].dt_txt).format(
@@ -29,24 +70,26 @@ function createFiveDayForecast() {
 }
 
 function createTab(cityName) {
-  tabLi.prepend(`
+  tabs.prepend(`
     <button
-      id="tab-${cityName}"
+      id="tab-${cityName.replace(' ', '').toLowerCase()}"
       type="button"
       role="tab"
       aria-selected="true"
-      aria-controls="tabpanel-${cityName}"
+      aria-controls="tabpanel-${cityName.replace(' ', '').toLowerCase()}"
     >
       <span class="focus">${cityName}</span>
     </button>
   `);
+}
 
+function createPanel(cityName) {
   $('main').prepend(`
     <div
-      id="tabpanel-${cityName}"
+      id="tabpanel-${cityName.replace(' ', '').toLowerCase()}"
       role="tabpanel"
       tabindex="0"
-      aria-labelledby="tab-${cityName}"
+      aria-labelledby="tab-${cityName.replace(' ', '').toLowerCase()}"
       class=""
     >
       <section tabindex="0" class="current-forecast">
@@ -69,59 +112,55 @@ function createTab(cityName) {
       </section>
     </div>
   `);
+}
+
+async function handleSearchButton(e) {
+  e.preventDefault();
+  const cityName = $('#search-form input').val() || 'Sacramento';
+
+  // loop through the cities in local storage that were set within the init function
+  // To check if the cityName being searched is already in their so we can return and not add it again
+  for (let i = 0; i < savedCities.length; i++) {
+    if (savedCities[i].name === cityName) {
+      alert('City is already Saved');
+      return;
+    }
+  }
+
+  const newCity = await fetchApi(cityName);
+
+  saveCity(newCity);
+
+  createTab(newCity.name);
+
+  //Create the tab panel for the city's weather.
+  createPanel(newCity.name);
+
+  // In the accessible.js file for making the keyboard navigation work with the tablist
   makeAccessible();
 }
 
-async function fetchApi(cityName) {
-  const response = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${API_KEY}`,
-  );
-  const data = await response.json();
-  const { lat, lon } = data[0];
+async function init() {
+  const storage = localStorage.getItem('forecast');
+  const parsedStorage = storage ? JSON.parse(storage) : [];
 
-  const fiveDayWeatherRes = await fetch(
-    `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`,
-  );
-
-  const currentWeatherRes = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`,
-  );
-
-  currentWeather = await currentWeatherRes.json();
-  const weather = await fiveDayWeatherRes.json();
-
-  // console.log(currentWeather);
-  // console.log(weather);
-  const currentPanelIds = [...$(`[role="tabpanel"]`)].map((val) => val.id);
-  // console.log(currentPanelIds);
-  if (currentPanelIds.includes(`tabpanel-${cityName}`)) {
-    return alert('City Already Searched');
-  }
-
-  fiveDayArray = weather.list.filter((day) => {
-    return day.dt_txt.includes('15:00:00');
+  parsedStorage.forEach((city) => {
+    savedCities.push(city);
   });
-  console.log(fiveDayArray);
 
-  createTab(weather.city.name);
+  savedCities.forEach(async (city) => {
+    const newCity = await fetchApi(city.name);
+
+    createTab(newCity.name);
+
+    createPanel(newCity.name);
+
+    makeAccessible();
+  });
+
+  searchButton.click(handleSearchButton);
 }
 
-searchButton.click((e) => {
-  e.preventDefault();
-
-  const input = $('#search-form input').val();
-
-  fetchApi(input ? input : 'Sacramento');
-  // if (!input.trim()) return alert('Please Try Again');
+$(() => {
+  init();
 });
-
-// Search Button Click  Function
-// Add Longitude and latitude to the localStorage based off of cityname.toLowerCase()
-// Create Tab and tabpanel Accordingly
-// reset Accessibility
-//
-
-// Tab Button Click Function
-// fetch Current Weather Data for the lon and lat saved to localStorage
-// save the data into a local variable .
-// empty out the panel tied to the tab button and redisplay the new data. (Make function to update Data based off panelId)
